528ad5cab0830a6097da0f8321263693ce2234b9
jwson-automation <sonjungwoo9@gmail.com>
2025-01-03 13:23:18 +0900

    add data

--- lib/models/lottie_state.dart
+++ lib/models/lottie_state.dart
@@ -24,6 +24,6 @@ class LottieState {
   }
 
   String get currentAnimation {
-    return isStudyPhase ? 'assets/lottie/mine.json' : 'assets/lottie/stop.json';
+    return isStudyPhase ? 'assets/lottie/critical.json' : 'assets/lottie/stop.json';
   }
 }
